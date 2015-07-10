445dae74c7119d4f11c07704f46b332e16d54053
Ahiknsr <gorantla.sai5@gmail.com>
2015-07-10 11:49:44 +0530

    switched api to https

--- igor_rest_api/management.py
+++ igor_rest_api/management.py
@@ -1,4 +1,5 @@
-from flask.ext.script import Manager
+from flask.ext.script import Server, Manager
+from OpenSSL import SSL
 
 from . import app
 from .api.auth.models import create_root_user
@@ -7,8 +8,12 @@ from .api.grouping.models import create_grouping_root_user
 from .config import ROOT_USER, ROOT_PASS
 from .db import db
 
-manager = Manager(app)
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('server.key')
+context.use_certificate_file('server.crt')
 
+manager = Manager(app)
+manager.add_command("runserver", Server(host="0.0.0.0", port=5000, ssl_context=context))
 
 @manager.command
 def init_db():
